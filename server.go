0e9a64c91bc14ff9de4b58eaa6fd265c4f1be235
wantux <wantux@gmail.com>
2024-03-13 08:34:43 +0200

    Update server.go

--- server.go
+++ server.go
@@ -21,7 +21,7 @@ import (
 	"strings"
 
 	hclog "github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/go-plugin/internal/grpcmux"
+	"github.com/pikselisbusiness/go-plugin/internal/grpcmux"
 	"google.golang.org/grpc"
 )
 
